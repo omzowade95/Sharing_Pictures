603f8d281488dbbe2f9d32495b3b4c6e37976db2
Oumar Wade <oumar951@outlook.fr>
2022-01-11 11:36:36 +0000

    Vue Login integrated

---
+++ src/main/java/com/example/sharing_pictures/controller/login/Login.java
@@ -0,0 +1,18 @@
+package com.example.sharing_pictures.controller.login;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class Login extends HttpServlet {
+    private static final String VUE_LOGIN = "/WEB-INF/website/auth/login.jsp";
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher(VUE_LOGIN).forward(request,response);
+    }
+}
