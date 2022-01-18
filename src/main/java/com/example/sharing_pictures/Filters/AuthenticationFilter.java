b5ddf1085f2dfabadfe7a8f09d803b272ada09c2
Oumar Wade <oumar951@outlook.fr>
2022-01-18 19:08:17 +0000

    Check Username Integrated

--- src/main/java/com/example/sharing_pictures/Filters/AuthenticationFilter.java
+++ src/main/java/com/example/sharing_pictures/Filters/AuthenticationFilter.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebFilter({"/About", "/photos", "/Albums/add","/Albums","/sidebar"})
+@WebFilter({"/Albums/add"})
 public class AuthenticationFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
